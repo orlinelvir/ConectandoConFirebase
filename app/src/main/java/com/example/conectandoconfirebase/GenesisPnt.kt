ad8ed290159fb7529324e23f9a4120cfcbeb2c35
Orlin Elvir <orlin.elvir@gmail.com>
2020-12-17 02:34:55 -0600

    Cambio de botones, gabinete Chronos listo

---
+++ app/src/main/java/com/example/conectandoconfirebase/GenesisPnt.kt
@@ -0,0 +1,16 @@
+package com.example.conectandoconfirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_genesis_pnt.cpuBtn
+
+class GenesisPnt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_genesis_pnt)
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/conectandoconfirebase/NeuronaPnt.kt
@@ -0,0 +1,15 @@
+package com.example.conectandoconfirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_neurona_pnt.cpuBtn
+
+class NeuronaPnt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_neurona_pnt)
+
+
+    }
+}
